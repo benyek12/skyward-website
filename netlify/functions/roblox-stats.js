26ffc21e4122296205a420cb0f46d26f1229fc41
Benyek_store <rendisn0@gmail.com>
2026-05-05 12:11:10 +0700

    test2

--- netlify/functions/roblox-stats.js
+++ netlify/functions/roblox-stats.js
@@ -2,36 +2,21 @@ const https = require('https');
 
 exports.handler = async (event) => {
   const { id } = event.queryStringParameters;
+  const url = `https://corsproxy.io/?https://games.roblox.com/v1/games?universeIds=${id}`;
 
   return new Promise((resolve) => {
-    const options = {
-      hostname: 'games.roblox.com',
-      path: `/v1/games?universeIds=${id}`,
-      method: 'GET',
-      headers: {
-        'User-Agent': 'Mozilla/5.0',
-        'Accept': 'application/json',
-        'Cookie': ''
-      }
-    };
-
-    const req = https.request(options, (res) => {
+    https.get(url, (res) => {
       let body = '';
       res.on('data', chunk => body += chunk);
       res.on('end', () => {
-        console.log('Status:', res.statusCode, 'Body:', body);
         resolve({
           statusCode: 200,
           headers: { 'Access-Control-Allow-Origin': '*' },
           body: body
         });
       });
-    });
-
-    req.on('error', (err) => {
+    }).on('error', (err) => {
       resolve({ statusCode: 500, body: JSON.stringify({ error: err.message }) });
     });
-
-    req.end();
   });
 };
